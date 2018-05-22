20e0e5f5d2b0d1b9f4249122cb5ead2eca5bdfe9
Александр Колесавин <my.android.369@gmail.com>
2018-05-22 16:47:47 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -6,26 +6,6 @@ const process = require('process')
 module.exports = (() => {
     const commandEmmiter = new EventEmitter()
 
-    function callApi(method, token, parameters) {
-        let url = `https://api.vk.com/method/${method}?access_token=${token}&v=5.74`
-
-        if (typeof parameters !== 'function') {
-            for (let param in parameters) {
-                url += `&${param}=${encodeURIComponent(parameters[param])}`
-            }
-        }
-
-        const callback = arguments[arguments.length - 1]
-
-        https.get(url, res => {
-            res.on('data', data => {
-                if (typeof callback === 'function') {
-                    callback(JSON.parse(data))
-                }
-            })
-        })
-    }
-
     function parseQuery(text, prefix) {
         const pattern = new RegExp(`${prefix}\\s?([А-Яа-яA-Za-z]+)(?:\\s([\\s\\S]+))?`, 'ui')
         return text.match(pattern)
@@ -50,7 +30,7 @@ module.exports = (() => {
         }
 
         listen() {
-            callApi('groups.getLongPollServer', this.token, { group_id: this.groupId }, data => {
+            this.callApi('groups.getLongPollServer', this.token, { group_id: this.groupId }, data => {
                 const { response } = data
                 if (response) {
                     console.log('Start listening...')
@@ -78,7 +58,8 @@ module.exports = (() => {
                                                 if (matches) {
                                                     for (let commandObj of self.commands) {
                                                         if (matches[1].toLowerCase() === commandObj.command) {
-                                                            commandObj.action(userId, matches[2].toLowerCase())
+                                                            let values = matches[2] ? matches[2].toLowerCase() : undefined
+                                                            commandObj.action(userId, values)
                                                         }
                                                     }
                                                 }
@@ -96,10 +77,30 @@ module.exports = (() => {
         }
 
         response(userId, message) {
-            callApi('messages.send', this.token, {
+            this.callApi('messages.send', this.token, {
                 user_id: userId,
                 message: message
             });
+        }
+
+        callApi(method, token, parameters) {
+            let url = `https://api.vk.com/method/${method}?access_token=${token}&v=5.74`
+
+            if (typeof parameters !== 'function') {
+                for (let param in parameters) {
+                    url += `&${param}=${encodeURIComponent(parameters[param])}`
+                }
+            }
+
+            const callback = arguments[arguments.length - 1]
+
+            https.get(url, res => {
+                res.on('data', data => {
+                    if (typeof callback === 'function') {
+                        callback(JSON.parse(data))
+                    }
+                })
+            })
         }
     }
 
